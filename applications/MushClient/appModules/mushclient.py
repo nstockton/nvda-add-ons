34f69baa1fff479997cbac6afbb8254ead154727
Nick Stockton <nstockton@users.noreply.github.com>
2016-06-21 14:32:08 -0700

    Hopefully fix mIRC and Mush Client add-ons sometimes failing to restore NVDA's original settings when app loses focus.

--- applications/MushClient/appModules/mushclient.py
+++ applications/MushClient/appModules/mushclient.py
@@ -3,7 +3,7 @@
 # A part of NonVisual Desktop Access (NVDA)
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
-# Copyright (C) 2014 Nick Stockton <nstockton@gmail.com>
+# Copyright (C) 2016 Nick Stockton <nstockton@gmail.com>
 
 """App module for Mush Client
 """
@@ -11,7 +11,7 @@
 
 # Built-in Python Modules
 from cStringIO import StringIO
-from os.path import join as pathJoin
+import os.path
 
 # Built-in NVDA Modules
 import addonHandler
@@ -27,49 +27,6 @@ import speech
 import textInfos
 from validate import Validator
 
-MUSHCLIENT_CONFIG = None
-MUSHCLIENT_CONFIG_SPEC = """
-[presentation]
-reportDynamicContentChanges = boolean(default=True)
-[reviewCursor]
-followCaret = boolean(default=False)
-[keyboard]
-speechInterruptForCharacters = boolean(default=False)
-speechInterruptForEnter = boolean(default=True)
-"""
-
-ORIGINAL_CONFIG = ConfigObj(indent_type="\t", default_encoding="utf-8", encoding="utf-8")
-ORIGINAL_CONFIG.newlines = "\r\n"
-ORIGINAL_CONFIG[u"presentation"] = {u"reportDynamicContentChanges": config.conf[u"presentation"][u"reportDynamicContentChanges"]}
-ORIGINAL_CONFIG[u"reviewCursor"] = {u"followCaret": config.conf[u"reviewCursor"][u"followCaret"]}
-ORIGINAL_CONFIG[u"keyboard"] = {
-	u"speechInterruptForCharacters": config.conf[u"keyboard"][u"speechInterruptForCharacters"],
-	u"speechInterruptForEnter": config.conf[u"keyboard"][u"speechInterruptForEnter"]
-}
-
-def loadMushclientConfig():
-	global MUSHCLIENT_CONFIG
-	if not MUSHCLIENT_CONFIG:
-		path = pathJoin(addonHandler.getCodeAddon().path, "config.ini")
-		MUSHCLIENT_CONFIG = ConfigObj(path, configspec=StringIO(MUSHCLIENT_CONFIG_SPEC), indent_type="\t", default_encoding="utf-8", encoding="utf-8", stringify=True)
-		MUSHCLIENT_CONFIG.newlines = "\r\n"
-		val = Validator()
-		result = MUSHCLIENT_CONFIG.validate(val, preserve_errors=True, copy=True)
-		if result != True:
-			log.warning("Corrupted MushClient add-on configuration file: %s", result)
-
-def saveMushclientConfig():
-	global MUSHCLIENT_CONFIG
-	if not MUSHCLIENT_CONFIG:
-		raise RuntimeError("Failed to load configuration file from the MushClient add-on folder.")
-	MUSHCLIENT_CONFIG[u"presentation"][u"reportDynamicContentChanges"] = config.conf[u"presentation"][u"reportDynamicContentChanges"]
-	MUSHCLIENT_CONFIG[u"reviewCursor"][u"followCaret"] = config.conf[u"reviewCursor"][u"followCaret"]
-	MUSHCLIENT_CONFIG[u"keyboard"][u"speechInterruptForCharacters"] = config.conf[u"keyboard"][u"speechInterruptForCharacters"]
-	MUSHCLIENT_CONFIG[u"keyboard"][u"speechInterruptForEnter"] = config.conf[u"keyboard"][u"speechInterruptForEnter"]
-	val = Validator()
-	MUSHCLIENT_CONFIG.validate(val, copy=True)
-	MUSHCLIENT_CONFIG.write()
-
 
 class Input(Window):
 	def event_gainFocus(self):
@@ -83,28 +40,18 @@ class Input(Window):
 			api.setNavigatorObject(output)
 			output.TextInfo.stripOuterWhitespace = True
 			self._output = output
-			if not MUSHCLIENT_CONFIG:
-				loadMushclientConfig()
-			config.conf[u"presentation"][u"reportDynamicContentChanges"] = MUSHCLIENT_CONFIG[u"presentation"][u"reportDynamicContentChanges"]
-			config.conf[u"reviewCursor"][u"followCaret"] = MUSHCLIENT_CONFIG[u"reviewCursor"][u"followCaret"]
-			config.conf[u"keyboard"][u"speechInterruptForCharacters"] = MUSHCLIENT_CONFIG[u"keyboard"][u"speechInterruptForCharacters"]
-			config.conf[u"keyboard"][u"speechInterruptForEnter"] = MUSHCLIENT_CONFIG[u"keyboard"][u"speechInterruptForEnter"]
 		else:
 			self._output = None
 
 	def event_loseFocus(self):
 		if self._output:
 			self._output.stopMonitoring()
-			if not MUSHCLIENT_CONFIG:
-				loadMushclientConfig()
-			saveMushclientConfig()
-			config.conf[u"presentation"][u"reportDynamicContentChanges"] = ORIGINAL_CONFIG[u"presentation"][u"reportDynamicContentChanges"]
-			config.conf[u"reviewCursor"][u"followCaret"] = ORIGINAL_CONFIG[u"reviewCursor"][u"followCaret"]
-			config.conf[u"keyboard"][u"speechInterruptForCharacters"] = ORIGINAL_CONFIG[u"keyboard"][u"speechInterruptForCharacters"]
-			config.conf[u"keyboard"][u"speechInterruptForEnter"] = ORIGINAL_CONFIG[u"keyboard"][u"speechInterruptForEnter"]
 
 
 class AppModule(appModuleHandler.AppModule):
+	mushclientConfig = None
+	originalConfig = {}
+
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		if obj.windowClassName == "AfxFrameOrView42s" and obj.windowControlID == 59648 and obj.IAccessibleRole == oleacc.ROLE_SYSTEM_CLIENT:
 			try:
@@ -112,25 +59,86 @@ class AppModule(appModuleHandler.AppModule):
 			except ValueError:
 				pass
 			clsList.insert(0, DisplayModelLiveText)
-		elif  obj.windowClassName == "Edit" and obj.windowControlID == 59664:
+		elif  obj.windowClassName == "Edit" and obj.windowControlID == 59664 and obj.IAccessibleRole == oleacc.ROLE_SYSTEM_TEXT:
 			clsList.insert(0, Input)
+
+	def event_appModule_gainFocus(self):
+		if self.mushclientConfig is not None:
+			# Don't clobber the already loaded config.
+			return
+		# Save original NVDA settings.
+		self.originalConfig["reportDynamicContentChanges"] = config.conf["presentation"]["reportDynamicContentChanges"]
+		self.originalConfig["followCaret"] = config.conf["reviewCursor"]["followCaret"]
+		self.originalConfig["speechInterruptForCharacters"] = config.conf["keyboard"]["speechInterruptForCharacters"]
+		self.originalConfig["speechInterruptForEnter"] = config.conf["keyboard"]["speechInterruptForEnter"]
+		# Define default values in case the configuration file doesn't exist.
+		configSpec = "\n".join((
+			"[presentation]",
+			"reportDynamicContentChanges = boolean(default=False)",
+			"[reviewCursor]",
+			"followCaret = boolean(default=False)",
+			"[keyboard]",
+			"speechInterruptForCharacters = boolean(default=False)",
+			"speechInterruptForEnter = boolean(default=True)"
+		))
+		# Load and validate the NVDA configuration file for MushClient.
+		path = os.path.join(addonHandler.getCodeAddon().path, "config.ini")
+		self.mushclientConfig = ConfigObj(path, configspec=StringIO(configSpec), indent_type="\t", default_encoding="utf-8", encoding="utf-8", stringify=True)
+		self.mushclientConfig.newlines = "\r\n"
+		val = Validator()
+		result = self.mushclientConfig.validate(val, preserve_errors=True, copy=True)
+		if result != True:
+			log.warning("Corrupted MushClient add-on configuration file: %s", result)
+			self.mushclientConfig = None
+			return
+		# Update NVDA settings from the MushClient configuration.
+		config.conf["presentation"]["reportDynamicContentChanges"] = self.mushclientConfig["presentation"]["reportDynamicContentChanges"]
+		config.conf["reviewCursor"]["followCaret"] = self.mushclientConfig["reviewCursor"]["followCaret"]
+		config.conf["keyboard"]["speechInterruptForCharacters"] = self.mushclientConfig["keyboard"]["speechInterruptForCharacters"]
+		config.conf["keyboard"]["speechInterruptForEnter"] = self.mushclientConfig["keyboard"]["speechInterruptForEnter"]
+
+	def event_appModule_loseFocus(self):
+		if self.mushclientConfig is None:
+			# Don't save what isn't there.
+			return
+		# Save the NVDA settings for MushClient.
+		self.mushclientConfig["presentation"]["reportDynamicContentChanges"] = config.conf["presentation"]["reportDynamicContentChanges"]
+		self.mushclientConfig["reviewCursor"]["followCaret"] = config.conf["reviewCursor"]["followCaret"]
+		self.mushclientConfig["keyboard"]["speechInterruptForCharacters"] = config.conf["keyboard"]["speechInterruptForCharacters"]
+		self.mushclientConfig["keyboard"]["speechInterruptForEnter"] = config.conf["keyboard"]["speechInterruptForEnter"]
+		val = Validator()
+		result = self.mushclientConfig.validate(val, preserve_errors=True, copy=True)
+		if result != True:
+			log.warning("Corrupted MushClient add-on configuration in memory: %s", result)
+			self.mushclientConfig = None
+			return
+		self.mushclientConfig.write()
+		self.mushclientConfig = None
+		# Restore the original NVDA settings.
+		config.conf["presentation"]["reportDynamicContentChanges"] = self.originalConfig["reportDynamicContentChanges"]
+		config.conf["reviewCursor"]["followCaret"] = self.originalConfig["followCaret"]
+		config.conf["keyboard"]["speechInterruptForCharacters"] = self.originalConfig["speechInterruptForCharacters"]
+		config.conf["keyboard"]["speechInterruptForEnter"] = self.originalConfig["speechInterruptForEnter"]
 
 	def script_review_bottom(self,gesture):
 		info = api.getReviewPosition().obj.makeTextInfo(textInfos.POSITION_LAST)
 		api.setReviewPosition(info.copy())
 		info.expand(textInfos.UNIT_LINE)
+		speech.cancelSpeech()
 		speech.speakTextInfo(info,unit=textInfos.UNIT_LINE,reason=speech.REASON_CARET)
 	script_review_bottom.__doc__=_("Moves the review cursor to the bottom line of the current navigator object and speaks it")
 
 	def script_toggle_interrupt_chars(self,gesture):
 		setting = not config.conf[u"keyboard"][u"speechInterruptForCharacters"]
 		config.conf[u"keyboard"][u"speechInterruptForCharacters"] = setting
+		speech.cancelSpeech()
 		speech.speakMessage("Interrupt on character press {state}.".format(state = "off" if not setting else "on"))
 	script_toggle_interrupt_chars.__doc__=_("Toggles the interrupting of speech when a character is pressed.")
 
 	def script_toggle_interrupt_enter(self,gesture):
 		setting = not config.conf[u"keyboard"][u"speechInterruptForEnter"]
 		config.conf[u"keyboard"][u"speechInterruptForEnter"] = setting
+		speech.cancelSpeech()
 		speech.speakMessage("Interrupt on enter press {state}.".format(state = "off" if not setting else "on"))
 	script_toggle_interrupt_enter.__doc__=_("Toggles the interrupting of speech when the enter key is pressed.")
 
--- applications/mIRC/appModules/mirc.py
+++ applications/mIRC/appModules/mirc.py
@@ -4,116 +4,99 @@
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 #Copyright (C) 2010 James Teh <jamie@jantrid.net>
-#Copyright (C) 2013 Nick Stockton <nstockton@gmail.com>
+#Copyright (C) 2016 Nick Stockton <nstockton@gmail.com>
 
 """App module for mIRC
 """
 
 
+# Built-in Python Modules
+from cStringIO import StringIO
+import os.path
+
+# Built-in NVDA Modules
 import addonHandler
 import appModuleHandler
+import nvdaBuiltin.appModules.mirc
 import api
-import config
-import controlTypes
 import oleacc
+import config
+from configobj import ConfigObj
+import NVDAObjects.IAccessible
 import speech
 import textInfos
-from NVDAObjects.window import Window, DisplayModelLiveText
-from NVDAObjects.IAccessible import StaticText
 from logHandler import log
-from configobj import ConfigObj
-from cStringIO import StringIO
 from validate import Validator
-from os.path import join as pathJoin
-
-MIRC_CONFIG = None
-MIRC_CONFIG_SPEC = """
-[presentation]
-reportDynamicContentChanges = boolean(default=True)
-[reviewCursor]
-followCaret = boolean(default=False)
-[keyboard]
-speechInterruptForCharacters = boolean(default=False)
-speechInterruptForEnter = boolean(default=False)
-"""
-
-ORIGINAL_CONFIG = ConfigObj(indent_type="\t", default_encoding="utf-8", encoding="utf-8")
-ORIGINAL_CONFIG.newlines = "\r\n"
-ORIGINAL_CONFIG[u"presentation"] = {u"reportDynamicContentChanges": config.conf[u"presentation"][u"reportDynamicContentChanges"]}
-ORIGINAL_CONFIG[u"reviewCursor"] = {u"followCaret": config.conf[u"reviewCursor"][u"followCaret"]}
-ORIGINAL_CONFIG[u"keyboard"] = {
-	u"speechInterruptForCharacters": config.conf[u"keyboard"][u"speechInterruptForCharacters"],
-	u"speechInterruptForEnter": config.conf[u"keyboard"][u"speechInterruptForEnter"]
-}
 
 
-def loadMircConfig():
-	global MIRC_CONFIG
-	if not MIRC_CONFIG:
-		path = pathJoin(addonHandler.getCodeAddon().path, "config.ini")
-		MIRC_CONFIG = ConfigObj(path, configspec=StringIO(MIRC_CONFIG_SPEC), indent_type="\t", default_encoding="utf-8", encoding="utf-8", stringify=True)
-		MIRC_CONFIG.newlines = "\r\n"
+class AppModule(nvdaBuiltin.appModules.mirc.AppModule):
+	mircConfig = None
+	originalConfig = {}
+
+	def event_NVDAObject_init(self, obj):
+		if isinstance(obj, NVDAObjects.IAccessible.IAccessible) and obj.windowClassName == "Static" and obj.windowControlID == 32918 and obj.IAccessibleRole == oleacc.ROLE_SYSTEM_STATICTEXT:
+			api.setNavigatorObject(obj)
+
+	def event_appModule_gainFocus(self):
+		if self.mircConfig is not None:
+			# Don't clobber the already loaded config.
+			return
+		# Save original NVDA settings.
+		self.originalConfig["reportDynamicContentChanges"] = config.conf["presentation"]["reportDynamicContentChanges"]
+		self.originalConfig["followCaret"] = config.conf["reviewCursor"]["followCaret"]
+		self.originalConfig["speechInterruptForCharacters"] = config.conf["keyboard"]["speechInterruptForCharacters"]
+		self.originalConfig["speechInterruptForEnter"] = config.conf["keyboard"]["speechInterruptForEnter"]
+		# Define default values in case the configuration file doesn't exist.
+		configSpec = "\n".join((
+			"[presentation]",
+			"reportDynamicContentChanges = boolean(default=True)",
+			"[reviewCursor]",
+			"followCaret = boolean(default=False)",
+			"[keyboard]",
+			"speechInterruptForCharacters = boolean(default=False)",
+			"speechInterruptForEnter = boolean(default=True)"
+		))
+		# Load and validate the NVDA configuration file for mIRC.
+		path = os.path.join(addonHandler.getCodeAddon().path, "config.ini")
+		self.mircConfig = ConfigObj(path, configspec=StringIO(configSpec), indent_type="\t", default_encoding="utf-8", encoding="utf-8", stringify=True)
+		self.mircConfig.newlines = "\r\n"
 		val = Validator()
-		result = MIRC_CONFIG.validate(val, preserve_errors=True, copy=True)
+		result = self.mircConfig.validate(val, preserve_errors=True, copy=True)
 		if result != True:
 			log.warning("Corrupted mIRC add-on configuration file: %s", result)
-
-def saveMircConfig():
-	global MIRC_CONFIG
-	if not MIRC_CONFIG:
-		raise RuntimeError("Failed to load configuration file from the mIRC add-on folder.")
-	MIRC_CONFIG[u"presentation"][u"reportDynamicContentChanges"] = config.conf[u"presentation"][u"reportDynamicContentChanges"]
-	MIRC_CONFIG[u"reviewCursor"][u"followCaret"] = config.conf[u"reviewCursor"][u"followCaret"]
-	MIRC_CONFIG[u"keyboard"][u"speechInterruptForCharacters"] = config.conf[u"keyboard"][u"speechInterruptForCharacters"]
-	MIRC_CONFIG[u"keyboard"][u"speechInterruptForEnter"] = config.conf[u"keyboard"][u"speechInterruptForEnter"]
-	val = Validator()
-	MIRC_CONFIG.validate(val, copy=True)
-	MIRC_CONFIG.write()
-
-
-class Input(Window):
-	def event_gainFocus(self):
-		super(Input, self).event_gainFocus()
-		try: output = self.parent.parent.lastChild.firstChild
-		except AttributeError: output = None
-		if isinstance(output, DisplayModelLiveText):
-			output.startMonitoring()
-			api.setNavigatorObject(output)
-			output.TextInfo.stripOuterWhitespace = True
-			self._output = output
-			if not MIRC_CONFIG:
-				loadMircConfig()
-			config.conf[u"presentation"][u"reportDynamicContentChanges"] = MIRC_CONFIG[u"presentation"][u"reportDynamicContentChanges"]
-			config.conf[u"reviewCursor"][u"followCaret"] = MIRC_CONFIG[u"reviewCursor"][u"followCaret"]
-			config.conf[u"keyboard"][u"speechInterruptForCharacters"] = MIRC_CONFIG[u"keyboard"][u"speechInterruptForCharacters"]
-			config.conf[u"keyboard"][u"speechInterruptForEnter"] = MIRC_CONFIG[u"keyboard"][u"speechInterruptForEnter"]
-		else:
-			self._output = None
-
-	def event_loseFocus(self):
-		if self._output:
-			self._output.stopMonitoring()
-			if not MIRC_CONFIG:
-				loadMircConfig()
-			saveMircConfig()
-			config.conf[u"presentation"][u"reportDynamicContentChanges"] = ORIGINAL_CONFIG[u"presentation"][u"reportDynamicContentChanges"]
-			config.conf[u"reviewCursor"][u"followCaret"] = ORIGINAL_CONFIG[u"reviewCursor"][u"followCaret"]
-			config.conf[u"keyboard"][u"speechInterruptForCharacters"] = ORIGINAL_CONFIG[u"keyboard"][u"speechInterruptForCharacters"]
-			config.conf[u"keyboard"][u"speechInterruptForEnter"] = ORIGINAL_CONFIG[u"keyboard"][u"speechInterruptForEnter"]
-
-
-class AppModule(appModuleHandler.AppModule):
-	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if obj.role == controlTypes.ROLE_WINDOW:
+			self.mircConfig = None
 			return
-		elif obj.windowClassName == "Static" and obj.windowControlID == 32918:
-			clsList.remove(StaticText)
-			clsList.insert(0, DisplayModelLiveText)
-		elif obj.windowClassName == "RichEdit20W" and obj.windowControlID == 32921:
-			clsList.insert(0, Input)
+		# Update NVDA settings from the mIRC configuration.
+		config.conf["presentation"]["reportDynamicContentChanges"] = self.mircConfig["presentation"]["reportDynamicContentChanges"]
+		config.conf["reviewCursor"]["followCaret"] = self.mircConfig["reviewCursor"]["followCaret"]
+		config.conf["keyboard"]["speechInterruptForCharacters"] = self.mircConfig["keyboard"]["speechInterruptForCharacters"]
+		config.conf["keyboard"]["speechInterruptForEnter"] = self.mircConfig["keyboard"]["speechInterruptForEnter"]
+
+	def event_appModule_loseFocus(self):
+		if self.mircConfig is None:
+			# Don't save what isn't there.
+			return
+		# Save the NVDA settings for mIRC.
+		self.mircConfig["presentation"]["reportDynamicContentChanges"] = config.conf["presentation"]["reportDynamicContentChanges"]
+		self.mircConfig["reviewCursor"]["followCaret"] = config.conf["reviewCursor"]["followCaret"]
+		self.mircConfig["keyboard"]["speechInterruptForCharacters"] = config.conf["keyboard"]["speechInterruptForCharacters"]
+		self.mircConfig["keyboard"]["speechInterruptForEnter"] = config.conf["keyboard"]["speechInterruptForEnter"]
+		val = Validator()
+		result = self.mircConfig.validate(val, preserve_errors=True, copy=True)
+		if result != True:
+			log.warning("Corrupted mIRC add-on configuration in memory: %s", result)
+			self.mircConfig = None
+			return
+		self.mircConfig.write()
+		self.mircConfig = None
+		# Restore the original NVDA settings.
+		config.conf["presentation"]["reportDynamicContentChanges"] = self.originalConfig["reportDynamicContentChanges"]
+		config.conf["reviewCursor"]["followCaret"] = self.originalConfig["followCaret"]
+		config.conf["keyboard"]["speechInterruptForCharacters"] = self.originalConfig["speechInterruptForCharacters"]
+		config.conf["keyboard"]["speechInterruptForEnter"] = self.originalConfig["speechInterruptForEnter"]
 
 	def script_review_bottom(self,gesture):
-		info=api.getReviewPosition().obj.makeTextInfo(textInfos.POSITION_LAST)
+		info = api.getReviewPosition().obj.makeTextInfo(textInfos.POSITION_LAST)
 		api.setReviewPosition(info.copy())
 		info.expand(textInfos.UNIT_LINE)
 		speech.cancelSpeech()
@@ -123,12 +106,14 @@ class AppModule(appModuleHandler.AppModule):
 	def script_toggle_interrupt_chars(self,gesture):
 		setting = not config.conf[u"keyboard"][u"speechInterruptForCharacters"]
 		config.conf[u"keyboard"][u"speechInterruptForCharacters"] = setting
+		speech.cancelSpeech()
 		speech.speakMessage("Interrupt on character press {state}.".format(state = "off" if not setting else "on"))
 	script_toggle_interrupt_chars.__doc__=_("Toggles the interrupting of speech when a character is pressed.")
 
 	def script_toggle_interrupt_enter(self,gesture):
 		setting = not config.conf[u"keyboard"][u"speechInterruptForEnter"]
 		config.conf[u"keyboard"][u"speechInterruptForEnter"] = setting
+		speech.cancelSpeech()
 		speech.speakMessage("Interrupt on enter press {state}.".format(state = "off" if not setting else "on"))
 	script_toggle_interrupt_enter.__doc__=_("Toggles the interrupting of speech when the enter key is pressed.")
 
