d4d1399c71af81b6248c13b41834f2b3f3f919be
Nick Stockton <nstockton@users.noreply.github.com>
2023-05-28 20:33:02 -0700

    Add a link in the ReadMe to the new IF Interpreters add-on repository, maintained by Noelia Ruiz Martínez. Remove the old IF Interpreter add-on code from this repository to avoid confusion.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nvda-add-ons
 My add-ons for the NVDA screen reader
+
+If you are looking for the IF Interpreters add-on, please note that Noelia Ruiz Martínez is the new maintainer of the add-on.
+You can find Noelia's repository for the IF Interpreters add-on [here.](https://github.com/nvdaes/ifInterpreters)
--- applications/IfInterpreters/appModules/heglk.py
+++
@@ -1,9 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-# A part of NonVisual Desktop Access (NVDA)
-# This file is covered by the GNU General Public License.
-# See the file COPYING for more details.
-# Copyright (C) 2020 Nick Stockton <nstockton@gmail.com>
-# Portions of This Work Copyright (C) 2006-2013 NV Access Limited
-
-# Local shared functions and classes
-from .winglk import *
